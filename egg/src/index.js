764c5b3f1a09cfe3fad91b7e022e886f1b2d0665
jerrynnm <jerry0302@kimo.com>
2025-06-06 15:37:14 +0800

    Delete egg/src/index.js

--- egg/src/index.js
+++
@@ -1,7 +0,0 @@
-// egg/src/index.js
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
-
-const root = createRoot(document.getElementById('root'));
-root.render(<App />);
