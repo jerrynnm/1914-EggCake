1946476d10d0ce0331be4b413031e51620f1caaa
jerrynnm <jerry0302@kimo.com>
2025-06-09 11:43:07 +0800

    Update firebase.js

--- egg/src/firebase.js
+++ egg/src/firebase.js
@@ -14,15 +14,16 @@ import {
 } from "firebase/firestore";
 
 // TODO: 用你的實際 config 值替換下面各欄位
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
-};
+// src/firebase.js
+console.log("🔥 Firebase config:", {
+  apiKey:             process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain:         process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL:        process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId:          process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket:      process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId:  process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId:              process.env.REACT_APP_FIREBASE_APP_ID,
+});
 
 // 初始化 App
 const app = initializeApp(firebaseConfig);
