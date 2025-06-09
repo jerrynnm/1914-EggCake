7cc02b10c4d7fd8054b0bc0b3829ad48fb4258df
jerrynnm <jerry0302@kimo.com>
2025-06-09 15:53:47 +0800

    Update OrderPage.jsx

--- egg/src/pages/OrderPage.jsx
+++ egg/src/pages/OrderPage.jsx
@@ -44,7 +44,7 @@ export default function OrderPage() {
 
     if (itemType === "原味") {
       if (plainCount <= 0) return alert("份數必須 ≥1");
-      items.push({ name: "份", qty: plainCount });
+      items.push({ name: "", qty: plainCount });
     }
 
     if (itemType === "特價綜合") {
